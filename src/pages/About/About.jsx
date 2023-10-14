2e75c041b68055e16d1637e35c3566c270be6ecc
Sardor <henkook-baron@mail.ru>
2023-10-14 11:32:27 +0500

    transleted about page

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -11,54 +11,76 @@ const About = () => {
   const info = [
     {
       id: 1,
-      text: `Maqsadim: Dasturlash yo'nalishida katta yutuqlarga erishish va nomdor dasturchi bo'lish. Bilganlarimni ulashish, bilmaganlarimni o'rganish.
-      Dasturlash orqali moliyaviy erkinlikka erishish.`,
+      text: `My goal: To make great progress in programming and become a famous programmer. Sharing what I know, learning what I don't know.
+      Achieving financial freedom through programming.`,
+      // text: `Maqsadim: Dasturlash yo'nalishida katta yutuqlarga erishish va nomdor dasturchi bo'lish. Bilganlarimni ulashish, bilmaganlarimni o'rganish.
+      // Dasturlash orqali moliyaviy erkinlikka erishish.`,
       img: 'darts.webp'
     },
     {
       id: 2,
-      text: `Qiziqishlarim: yangi texnologiyalar o'rganish, ish jarayonida duch keladigan masalalarga yechim topish, dasturlash olamidagi yangiliklardan xabardor bo'lish.`,
+      text: `My interests: learning new technologies, finding solutions to problems encountered in the work process, being aware of the news in the world of programming.`,
+      // text: `Qiziqishlarim: yangi texnologiyalar o'rganish, ish jarayonida duch keladigan masalalarga yechim topish, dasturlash olamidagi yangiliklardan xabardor bo'lish.`,
       img: 'code.webp'
     },
     {
       id: 3,
-      text: `Hobbiyim: musiqa eshitib kod yozish, dasturlashga odi videolar ko'rish, avtomobillarni o'rganish va boshqalar.`,
+      text: `Hobbies: coding while listening to music, watching programming videos, studying cars, etc.`,
+      // text: `Hobbiyim: musiqa eshitib kod yozish, dasturlashga odi videolar ko'rish, avtomobillarni o'rganish va boshqalar.`,
       img: 'hobby.webp'
     },
     {
       id: 4,
-      text: `1994-yil 3-yanvar kuni Far'g'ona viloyatida tug'ilganman. 1996-yildan beri Toshkent viloyati Chirchiq shahrida istiqomat qilaman.`,
+      text: `I was born on January 3, 1994 in Fergana region. Since 1996, I have been living in Chirchik, Tashkent region.`,
+      // text: `1994-yil 3-yanvar kuni Far'g'ona viloyatida tug'ilganman. 1996-yildan beri Toshkent viloyati Chirchiq shahrida istiqomat qilaman.`,
       img: 'sardor02.webp'
     },
     {
       id: 5,
+      text: `In 2001, I went to secondary school No. 11 in Chirchik city, and in 2010 I graduated with excellent grades. In the 8th-9th grades, I participated in the Olympiads in informatics and reached the regional stages.`,
       text: `2001-yil Chirchiq shahar 11-sonli o'rta umumta'lim maktabiga borib, 2010-yil maktabni a'lo baholarga tamomlaganman. 8-9 sinflarda informatika fanidan olimpiadalarda ishtirok etib, Viloyat bosqichlariga chiqqanman.`,
       img: 'school.webp'
     },
     {
       id: 6,
-      text: `2010-yil Chirchiq sanoat kasb-hunar kollejining "Avtomobillarga texnik-xizmat ko'rsatish" yo'nalishi bo'yicha o'qishga kirib, o'qishni 2013-yil a'lo baholarga tamomlaganman.`,
+      text: `In 2010, I entered the Chirchik Industrial Vocational College, majoring in "Technical maintenance of automobiles", and graduated with honors in 2013..`,
+      // text: `2010-yil Chirchiq sanoat kasb-hunar kollejining "Avtomobillarga texnik-xizmat ko'rsatish" yo'nalishi bo'yicha o'qishga kirib, o'qishni 2013-yil a'lo baholarga tamomlaganman.`,
       img: 'college.webp'
     },
     {
       id: 7,
-      text: `2014-yil "Петербургский государственный университет путей сообщения Императора Александра I" universitetining "Строительство железных дорог, мостов и транспортных тоннелей" fakultetiga o'qishga kirdim.`,
+      text: `In 2014, I entered the faculty of "Construction of railways, bridges and transport tunnels" at the "Saint-Petersburg State University of Transport of Emperor Alexander I".`,
+      // text: `2014-yil "Петербургский государственный университет путей сообщения Императора Александра I" universitetining "Строительство железных дорог, мостов и транспортных тоннелей" fakultetiga o'qishga kirdim.`,
       img: 'pgups.webp'
     },
     {
       id: 8,
-      text: `2013-yildan 2020-yilgacha Sankt-Peterburgda yashab ishladim`,
+      text: `From 2013 to 2020, I lived and worked in Saint-Petersburg`,
+      // text: `2013-yildan 2020-yilgacha Sankt-Peterburgda yashab ishladim`,
       img: 'spb.webp'
     },
     {
       id: 9,
-      text: `2020-yil yakunida O'zbekistonga qaytib kelib 2021-yildan 2022-yilni boshigacha "ElektroSetMontaj" firmasida ish boshqaruvchi lavozimida ishladim. Ishdan bo'sh vaqtim "IT-Expert" o'quv markazida frontendni o'rganishni boshladim.`,
+      text: `At the end of 2020, I returned to Uzbekistan and from 2021 to the beginning of 2022 I worked as a manager at the "ElektroSetMontaj" company. In my spare time, I started learning frontend at the education center "IT-Expert".`,
+      // text: `2020-yil yakunida O'zbekistonga qaytib kelib 2021-yildan 2022-yilni boshigacha "ElektroSetMontaj" firmasida ish boshqaruvchi lavozimida ishladim. Ishdan bo'sh vaqtim "IT-Expert" o'quv markazida frontendni o'rganishni boshladim.`,
       img: 'uzbmap.webp'
     },
     {
       id: 10,
-      text: `2022-yilning noyabr oyida Najot Ta'lim o'quv markazining Full-stack dasturlash" yo'nalishiga o'qishga kirdim va hamma imtihonlardan eng yuqori o'rinlarda o'tdim.`,
+      text: `In November 2022, I enrolled in the "Full-stack web programming" course of the "Najot ta'lim" education center and passed all the exams with the highest marks.`,
+      // text: `2022-yilning noyabr oyida Najot Ta'lim o'quv markazining Full-stack dasturlash" yo'nalishiga o'qishga kirdim va hamma imtihonlardan eng yuqori o'rinlarda o'tdim.`,
       img: 'nt.webp'
+    },
+    {
+      id: 11,
+      text: `I completed the course in October 2023 and was awarded a certificate.`,
+      // text: `2023-yil oktyabr oyida o'quv kursini tugatdim sertifikat qilan taqdirlandim.`,
+      img: 'sertificate_nt.webp'
+    },
+    {
+      id: 12,
+      text: `Now I'm looking for a job and I hope I'm the person you're looking for.`,
+      img: 'job.webp'
     },
   ];
 
@@ -143,7 +165,7 @@ const About = () => {
               />
             </div>
 
-            <h2>Davomi endi yoziladi...😉 </h2>
+            <h2>to be continued...</h2>
           </div>
 
         </div>
