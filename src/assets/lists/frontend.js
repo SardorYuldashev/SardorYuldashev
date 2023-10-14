fcdce17854d7e23ad882a6efad7572ee1a29a4ab
Sardor <henkook-baron@mail.ru>
2023-10-14 11:03:08 +0500

    translate description projects

--- src/assets/lists/frontend.js
+++ src/assets/lists/frontend.js
@@ -3,70 +3,70 @@ const frontend = [
     id: 1,
     image: 'portfolio.webp',
     title: 'Old Portfolio',
-    technologies: "Bu eski portfolio bolib, bu yerda background uchun html-canvasdan foydalanilgan. Media-querylar yozilgan. Parallax effect tadbiq etilgan. JS fayl ham qo'llanilgan",
+    technologies: "This is an old portfolio that uses html-canvas for the background. Media queries are written. Parallax effect applied. JS file is also used.",
     url: 'https://sardoryuldashev.github.io/Portfolio/'
   },
   {
     id: 2,
     image: 'techzone.webp',
     title: 'TechZone',
-    technologies: "Bu loyiha sahifalaridan birida Youtube va Instagram UIni kichik nusxalari displeyga joylangan. Animatsiya popuplardan foydalanilgan",
+    technologies: "On the pages of this project, small copies of the YouTube and Instagram UI are displayed. Animations and popups are used.",
     url: 'https://sardoryuldashev.github.io/TechZone/'
   },
   {
     id: 3,
     image: 'uzb.webp',
     title: 'Uzbekistan',
-    technologies: `O'zbekistonning interaktiv xaritasi. O'zingigzga kerakli viloyat ustiga bosib, viloyat haqidagi ma'lumotlarni ko'ra olasiz`,
+    technologies: `Interactive map of Uzbekistan. You can click on the region you need and see the information about the region.`,
     url: 'https://sardoryuldashev.github.io/Uzbekistan/'
   },
   {
     id: 4,
     image: 'restaurant.webp',
     title: 'Restaurant',
-    technologies: "Menyuni ko'rish uchun ro'yxatdan o'tiladi yoki profilga kiriladi. Hamma ro'yxatdan o'tgan foydalanuvchilar ma'lumotlari localStorageda saqlab qo'yiladi",
+    technologies: "Register or log in to view the menu. All registered user data is stored in localStorage.",
     url: 'https://sardoryuldashev.github.io/Restaurant/'
   },
   {
     id: 5,
     image: 'mario.webp',
     title: 'Mario animation',
-    technologies: "HTML, CSS va JS yordamida yasalgan Mario o'yinini imitatsiya qiluvchi qisqa animatsion rolik.",
+    technologies: "A short animated video simulating a Mario game made using HTML, CSS and JS.",
     url: 'https://sardoryuldashev.github.io/Mario/'
   },
   {
     id: 6,
     image: 'mymemories.webp',
     title: 'MyMemories',
-    technologies: "Loyihada ro'yxatdan o'tish va profilga kirish imkoniyati mavjud. Foydalanuvchi o'z qaydlarini yozib borishi, o'chirishi, tahrirlashi, qaydlarni izlashi,  qaydlarni yoqqan va yoqmaganga ajratishi mumkin",
+    technologies: "The project has the possibility to register and log in. The user can record, delete, edit, search for notes, and sort notes into likes and dislikes.",
     url: 'https://site-my-memories.netlify.app/'
   },
   {
     id: 7,
     image: 'formen.webp',
     title: 'for Men',
-    technologies: "Erkaklar uchun internet do'kon. Maxsuotlarni kategoriyasiga qarab, narxiga qarab va nomiga qarab izlash mumkin. o'sish va kamayish tartibida tartiblash imkoniyati mavjud",
+    technologies: "Online store for men. Products can be searched by category, price, and name. there is an option to sort in ascending and descending order.",
     url: 'https://sardoryuldashev.github.io/forMen/'
   },
   {
     id: 8,
     image: 'odinodi.webp',
     title: 'ODInODI',
-    technologies: "Ayollar uchun internet magazin. Shaxsiy dizayn.",
+    technologies: "Online magazine for women. Personal design.",
     url: 'https://sardoryuldashev.github.io/ODInODI/'
   }, 
   {
     id: 9,
     image: 'cardrive.webp',
     title: 'Car Drive',
-    technologies: "JavaScript Class-constructor yordamida yasalgan avtomobil mini simulyatori.",
+    technologies: "A car mini simulator made using JavaScript Class-constructor.",
     url: 'https://car-drive.netlify.app/'
   },       
   {
     id: 10,
     image: 'coral.webp',
     title: 'Coral',
-    technologies: "Internet do'kon. Qidirish, sortlash imkoniyatlariga ega",
+    technologies: "Online store. It has search and sorting capabilities.",
     url: 'https://sardoryuldashev.github.io/Coral/'
   },
   {
@@ -80,21 +80,21 @@ const frontend = [
     id: 12,
     image: 'dreamHouse.webp',
     title: 'Dream House',
-    technologies: "Bu loyihada SVG yordamida interaktiv xarita tadbiq qilingan. 360 gradusli rasmlarni hamda 360 gradusli 3D modelni ko'rish mumkin.",
+    technologies: "This project implements an interactive map using SVG. 360-degree images and 360-degree 3D models can be viewed.",
     url: 'https://sardoryuldashev.github.io/Dream-House/'
   },
   {
     id: 13,
     image: 'chat.webp',
     title: 'Chat',
-    technologies: "JavaScript yordamida yaratilgan kichik bir chat",
+    technologies: "A small chat created using JavaScript.",
     url: 'https://sardoryuldashev.github.io/Chat/'
   },  
   {
     id: 14,
     image: 'burger.webp',
     title: 'Buegr',
-    technologies: "JavaScript yordamida yasalgan gamburger yig'ish simulyatori. Kerakli ingridientlarni qo'shish, ortiqchasini o'chirish, yakunida buyurtma berish mumkin",
+    technologies: "A hamburger assembly simulator made with JavaScript. You can add the necessary ingredients, delete the excess, and finally order.",
     url: 'https://sardoryuldashev.github.io/Gamburger/'
   },  
 ];
--- src/assets/lists/pet.js
+++ src/assets/lists/pet.js
@@ -6,13 +6,13 @@ const pet = [
     technologies: `Here react.js is used for the frontend, and expres.js and knex query builder for the backend. Database - PostgreSQL`,
     url: 'https://guide-board.netlify.app'
   },
-  {
-    id: 2,
-    image: 'rooms.webp',
-    title: 'Rooms',
-    technologies: `GraphQL va ApolloClient yordamida yasalgan pet projekt. Ma'lumotlaar ombori: PostgreSQL. CRUD. Search tadbiq qilingan.`,
-    url: '!#'
-  },
+  // {
+  //   id: 2,
+  //   image: 'rooms.webp',
+  //   title: 'Rooms',
+  //   technologies: `GraphQL va ApolloClient yordamida yasalgan pet projekt. Ma'lumotlaar ombori: PostgreSQL. CRUD. Search tadbiq qilingan.`,
+  //   url: '!#'
+  // },
 ];
 
 export default pet;
